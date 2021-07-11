1ec38fb66f641199e72743ece89c93b30d0e9d2b
AbhinavRana <vishuchoudhary131@gmail.com>
2021-07-11 21:59:58 +0530

    updated code

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 
       path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
 
-    path('/',include('blog.urls')),
+    path('',include('blog.urls')),
     path('profile/',user_views.profile,name='profile'),
 ]
 if settings.DEBUG:

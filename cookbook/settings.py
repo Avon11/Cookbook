798a03031a563efcb28acd0e73ed4b4488ba0543
AbhinavRana <vishuchoudhary131@gmail.com>
2021-07-12 18:20:40 +0530

    Update v4

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,4 +141,3 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='amonguskapapa@gmail.com'
 EMAIL_HOST_PASSWORD='tykjyoqnftyqslzz'
-django_heroku.settings(locals())

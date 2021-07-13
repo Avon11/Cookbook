1fef056f4efe681396441af98f9056d1616f035f
Abhi Rana <63769307+Avon11@users.noreply.github.com>
2021-07-13 13:42:43 +0530

    Update settings.py

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4q3ex8&%3s+h)!foqdf@*$yja5%=px!8$%arefcdm1l3_$isq@'
+SECRET_KEY = **HIDDEN**
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,4 +140,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='amonguskapapa@gmail.com'
-EMAIL_HOST_PASSWORD='tykjyoqnftyqslzz'
+EMAIL_HOST_PASSWORD=**HIDDEN**
